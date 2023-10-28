f28882a9170e9a1b8f25f6a34e3e23922d5372cc
baka9131 <baka9131@gmail.com>
2023-10-28 10:13:21 +0900

    feat/add_provider_add_colorpicker

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:inha_capstone_project_byoa/data/provider.dart';
+import 'package:provider/provider.dart';
 import 'route/route.dart';
 
 void main() {
   runApp(
-    MaterialApp.router(
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
+    // 상태관리를 위해 Provider 사용
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => Properties(
+            width: 100,
+            height: 100,
+            color: Colors.blue,
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        debugShowCheckedModeBanner: false,
+        routerConfig: route,
       ),
-      debugShowCheckedModeBanner: false,
-      routerConfig: route,
     ),
   );
 }
